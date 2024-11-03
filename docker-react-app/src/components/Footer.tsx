fcbc4313bf5286aeb6f4650c3fe45b1d45c7846c
saiprabhu-dandanayak <saiprabhu.dandanayak@zemosolabs.com>
2024-11-03 21:35:39 +0530

    🚚feat: dockerized basic react application

---
+++ docker-react-app/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <p>© 2024 Docker React App</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ docker-react-app/src/components/MainContent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainContent: React.FC = () => {
+  return (
+    <main>
+      <p>This is a simple docker app with a header, main content, and footer.</p>
+    </main>
+  );
+};
+
+export default MainContent;
